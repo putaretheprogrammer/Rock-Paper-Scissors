6fc9700d3bd0a1fc60a95f2bedfc13a7caeee4a9
Putare <60327078+putaretheprogrammer@users.noreply.github.com>
2020-11-09 20:21:13 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,168 +1,86 @@
 import random # imports random
-from random import randint #imports randint
-import time # imports ti
-randomList = ["Rock", "Paper", "Scissors"]
-
-playerScore = 0 # if playerScore goes up to 3 then player wins
-comScore = 0 # if comScore goes up to 3 then computer wins
+from random import randint # imports randint
+import time # imports time
 rounds = 0
+playerScore = 0
+comScore = 0
 
-# round 1
-print("Rock, Paper, or Scissors? Computer: " + str(comScore) + " Player: " + str(playerScore))
-playerChoice = input()
-comChoice = random.choice(randomList)
-if playerChoice == "Rock":
-    if comChoice == "Paper":
-        print("Computer Wins Round 1")
-        comScore = comScore + 1
+def CallRound():
+    global playerScore # if playerScore goes up to 3 then player wins
+    global comScore # if comScore goes up to 3 then computer wins
+    global rounds
+
+    randomList = ["Rock", "Paper", "Scissors"]
+        
+    
+    # round
+    if rounds == 0:
         rounds = rounds + 1
-    elif comChoice == "Scissors":
-        print("Player Wins Round 1")
-        playerScore = playerScore + 1
+        print("Round " + str(rounds))
     else:
-        print("Tie! Both won round 1!")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-elif playerChoice == "Paper":
-     if comChoice == "Scissors":
-         print("Computer Wins Round 1")
-         comScore = comScore + 1
-         rounds = rounds + 1
-     elif comChoice == "Rock":
-         print("Player Wins Round 1")
-         playerScore = playerScore + 1
-         rounds = rounds + 1
-     else:
-         print("Tie! Both won round 1!")
-         playerScore = playerScore + 1
-         comScore = comScore + 1
-         rounds = rounds
-elif playerChoice == "Scissors":
-    if comChoice == "Rock":
-         print("Computer Wins Round 1")
-         comScore = comScore + 1
-         rounds = rounds + 1
-    elif comChoice == "Paper":
-        print("Player Wins Round 1")
-        playerScore = playerScore + 1
-        rounds = rounds + 1
+        print("Round " + str(rounds))
+        
+    print("Rock, Paper, or Scissors? Computer: " + str(comScore) + " Player: " + str(playerScore))
+    playerChoice = input()
+    comChoice = random.choice(randomList)
+    if playerChoice == "Rock":
+        if comChoice == "Paper":
+            print("Computer Wins Round " + str(rounds))
+            comScore = comScore + 1
+            rounds = rounds + 1
+        elif comChoice == "Scissors":
+            print("Player Wins Round " + str(rounds))
+            playerScore = playerScore + 1
+            rounds = rounds + 1
+        else:
+            print("Tie! Both won round 1!")
+            playerScore = playerScore + 1
+            comScore = comScore + 1
+            rounds = rounds + 1
+    elif playerChoice == "Paper":
+         if comChoice == "Scissors":
+             print("Computer Wins Round 1")
+             comScore = comScore + 1
+             rounds = rounds + 1
+         elif comChoice == "Rock":
+             print("Player Wins Round 1")
+             playerScore = playerScore + 1
+             rounds = rounds + 1
+         else:
+             print("Tie! Both won round 1!")
+             playerScore = playerScore + 1
+             comScore = comScore + 1
+             rounds = rounds + 1
+    elif playerChoice == "Scissors":
+        if comChoice == "Rock":
+             print("Computer Wins Round 1")
+             comScore = comScore + 1
+             rounds = rounds + 1
+        elif comChoice == "Paper":
+            print("Player Wins Round 1")
+            playerScore = playerScore + 1
+            rounds = rounds + 1
+        else:
+            print("Tie! Both won round 1! ")
+            playerScore = playerScore + 1
+            comScore = comScore + 1
+            rounds = rounds + 1
     else:
-        print("Tie! Both won round 1! ")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-        rounds = rounds
-else:
-    print("Your choice was invalid.")
-    input()
-    exit()
+        print("Your choice was invalid.")
+        input()
+        exit()
 
-# round 2
-print("Rock, Paper, or Scissors? Computer: " + str(comScore) + " Player: " + str(playerScore))
-playerChoice = input()
-comChoice = random.choice(randomList)
-if playerChoice == "Rock":
-    if comChoice == "Paper":
-        print("Computer Wins Round 2")
-        comScore = comScore + 1
-        rounds = rounds + 1
-    elif comChoice == "Scissors":
-        print("Player Wins Round 2")
-        playerScore = playerScore + 1
-    else:
-        print("Tie! No one won this round!")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-elif playerChoice == "Paper":
-     if comChoice == "Scissors":
-         print("Computer Wins Round 2")
-         comScore = comScore + 1
-         rounds = rounds + 1
-     elif comChoice == "Rock":
-         print("Player Wins Round 2")
-         playerScore = playerScore + 1
-         rounds = rounds + 1
-     else:
-         print("Tie! No one won this round!")
-elif playerChoice == "Scissors":
-    if comChoice == "Rock":
-         print("Computer Wins Round 2")
-         comScore = comScore + 1
-         rounds = rounds + 1
-    elif comChoice == "Paper":
-        print("Player Wins Round 2")
-        playerScore = playerScore + 1
-        rounds = rounds + 1
-    else:
-        print("Tie! Both won round 2! ")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-        rounds = rounds
-else:
-    print("Your choice was invalid.")
-    input()
-    exit()
+print("How many rounds?")
+roundCount = int(input())
 
-#round 3
-print("Rock, Paper, or Scissors? Computer: " + str(comScore) + " Player: " + str(playerScore))
-playerChoice = input()
-comChoice = random.choice(randomList)
-if playerChoice == "Rock":
-    if comChoice == "Paper":
-        print("Computer Wins Round 3")
-        comScore = comScore + 1
-        rounds = rounds + 1
-    elif comChoice == "Scissors":
-        print("Player Wins Round 3")
-        playerScore = playerScore + 1
-    else:
-        print("Tie! No one won this round!")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-elif playerChoice == "Paper":
-     if comChoice == "Scissors":
-         print("Computer Wins Round 3")
-         comScore = comScore + 1
-         rounds = rounds + 1
-     elif comChoice == "Rock":
-         print("Player Wins Round 3")
-         playerScore = playerScore + 1
-         rounds = rounds + 1
-     else:
-         print("Tie! No one won this round!")
-elif playerChoice == "Scissors":
-    if comChoice == "Rock":
-         print("Computer Wins Round 3")
-         comScore = comScore + 1
-         rounds = rounds + 1
-    elif comChoice == "Paper":
-        print("Player Wins Round 3")
-        playerScore = playerScore + 1
-        rounds = rounds + 1
-    else:
-        print("Tie! Both won round 3! ")
-        playerScore = playerScore + 1
-        comScore = comScore + 1
-        rounds = rounds
-else:
-    print("Your choice was invalid.")
-    input()
-    exit()
-
-# time to show the results
-print("=============================================")
-time.sleep(2)
-if playerScore > comScore:
-    print("Player has won the game!")
-    input()
-    exit()
-elif playerScore < comScore:
-    print("Computer has won the game!")
-    input()
-    exit()
-else:
-    print("Tie! Both has won!")
-    input()
-    exit()
-
-
-
+for i in range(roundCount):
+    CallRound()
+    while playerScore == 3 or (playerScore > comScore and rounds == 3):
+        print("Player wins!")
+        input()
+        exit()
+    while comScore == 3 or comScore == 3 and rounds == 3:
+        print("Computer wins!")
+        input()
+        exit()
+    
